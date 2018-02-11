455aa071da804371fde3f92d99f30410dd23d1eb
yong <yongzhian@163.com>
2018-02-11 15:27:26 +0800

    BeanAware测试

--- src/test/java/cn/zain/aware/BeanAware.java
+++ src/test/java/cn/zain/aware/BeanAware.java
@@ -1,17 +1,18 @@
 package cn.zain.aware;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ResourceLoaderAware;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 
 /**
@@ -22,8 +23,20 @@ import org.springframework.stereotype.Component;
 @ComponentScan("cn.zain.aware")
 public class BeanAware implements BeanNameAware, ResourceLoaderAware {
 
+    private Logger logger = LoggerFactory.getLogger(BeanAware.class);
+
     private String beanName;
     private ResourceLoader loader;
+
+    public void outputResult() {
+        logger.info("bean 名称:{}",beanName);
+        Resource resource = loader.getResource("classpath:test.txt");
+        try {
+            logger.info("加载文件:{}", IOUtils.toString(resource.getInputStream(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void setBeanName(String beanName) {
